5f320949331c3a84a0fc8f4d1969064c977867b0
Serhii Sokolov <sokolovserega1982@gmail.com>
2022-01-13 08:56:17 +0200

    added destructurization at file Form.jsx

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -23,6 +23,7 @@ export default class Form extends Component {
   };
 
   render() {
+    const { name, number } = this.state;
     return (
       <FormTag onSubmit={this.handleSubmit}>
         <label>
@@ -32,7 +33,7 @@ export default class Form extends Component {
             name="name"
             pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
             title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-            value={this.state.name}
+            value={name}
             onChange={this.handleChange}
             required
           />
@@ -44,7 +45,7 @@ export default class Form extends Component {
             name="number"
             pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
             title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-            value={this.state.number}
+            value={number}
             onChange={this.handleChange}
             required
           />
